79030240115f81c565ca6e8d8b452f10764639f3
Paige Davenport <davenport.paige@outlook.com>
2019-09-04 14:51:55 -0700

    Added functionality to DateRange to raise exceptions for invalid dates and corresponding tests.

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -5,8 +5,17 @@ module Hotel
     attr_reader :check_in, :check_out
 
     def initialize(check_in, check_out)
-      @check_in = Date.strptime(check_in, "%m-%d-%Y")
-      @check_out = Date.strptime(check_out, "%m-%d-%Y")
+      check_in = Date.strptime(check_in, "%m-%d-%Y")
+      check_out = Date.strptime(check_out, "%m-%d-%Y")
+
+      if check_in === check_out
+        raise ArgumentError.new("Check-in date (#{check_in}) is equal to check-out date (#{check_out}).")
+      elsif check_in > check_out
+        raise ArgumentError.new("Check-in (#{check_in}) date cannot be after check-out date (#{check_out}).")
+      else
+        @check_in = check_in
+        @check_out = check_out
+      end
     end
     
     #will check if date is in a date range
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -1,7 +1,7 @@
 require_relative 'test_helper'
 
 describe "DateRange" do
-  describe "Initialize" do
+  describe "initialize" do
     before do
       check_in = "04-05-2020"
       check_out = "04-16-2020"
@@ -27,10 +27,23 @@ describe "DateRange" do
     end
   end
   
+  describe "valid_range?" do
+    it "raises an ArgumentError when check_out date is before check_in date" do
+      expect{ Hotel::DateRange.new("03-04-2020", "03-03-2020") }.must_raise ArgumentError
+    end
+
+    it "raises an ArgumentError if check_out and check_in date are the same" do
+      expect{ Hotel::DateRange.new("03-04-2020", "03-04-2020") }.must_raise ArgumentError
+    end
+
+    it "raises an ArgumentError if check_in/check_out date is not in a valid format" do
+      expect{ Hotel::DateRange.new("Monday", "03-04-2020") }.must_raise ArgumentError
+      expect{ Hotel::DateRange.new("03-2020", "03-04-2020") }.must_raise ArgumentError
+    end
+  end
+  
   # it "calculates the number of nights in the range" do
   # end
-
-  #TODO write test for valid date
 
 end
 
