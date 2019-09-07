5ce999d918a0192a3e85002fd29254c0b4cf0041
Paige Davenport <davenport.paige@outlook.com>
2019-09-07 13:37:12 -0700

    Refactored find room method to find 1 or multiple rooms.

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -41,22 +41,48 @@ module Hotel
       return available_rooms
     end
 
-    def find_room(available_rooms)
-      available_rooms[0]
+    def find_rooms(available_rooms, total_rooms = 1)
+      if total_rooms == 1
+        return available_rooms[0]
+      else
+        rooms = []
+        total_rooms.times do
+          rooms << available_rooms.shift
+        end
+        return rooms
+      end
     end
 
     def book_single_res(check_in, check_out)
-      dates = Hotel::DateRange.new(check_in, check_out)
       available_rooms = rooms_available(check_in, check_out)
+      dates = Hotel::DateRange.new(check_in, check_out)
       
       if available_rooms.length == 0
         raise ArgumentError.new("No available rooms for #{check_in} - #{check_out}")
       else
-        room = find_room(available_rooms)
+        room = find_rooms(available_rooms)
         reservations << Hotel::SingleRes.new(dates, room)
       end
     end
 
-    #method for booking a BlockRes
+    # def book_block_res(check_in:, check_out:, total_rooms:, group:, discount:)
+    #   # raises error if try to set aside more than 5 rooms
+    #   if total_rooms > 5
+    #     raise ArgumentError.new("Total rooms in block cannot exceed 5 rooms.")
+    #   end
+
+    #   available_rooms = rooms_available(check_in, check_out)
+    #   dates = Hotel::DateRange.new(check_in, check_out)
+
+    #   if available_rooms.length < total_rooms
+    #     raise ArgumentError.new("Insufficient rooms available for #{check_in} - #{check_out}.")
+    #   else
+    #     rooms = []
+    #     total_rooms.times do 
+    #       rooms << find_room(available_rooms)
+    #     end
+      
+
+    # end
   end
 end
